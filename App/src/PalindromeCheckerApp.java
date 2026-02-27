dc0089e92a6cee2b5d38e9c665e9e1d52dd6b4b8
sf6835srm <sf6835@srmist.edu.in>
2026-02-27 13:25:56 +0530

    Delete App directory

--- App/src/PalindromeCheckerApp.java
+++
@@ -1,8 +0,0 @@
-public class PalindromeCheckerApp {
-    static void main() {
-        System.out.println("Welcome to PalindromeCheckerApp");
-        System.out.println("Version 1.0");
-        System.out.println("System Initialised Successfully");
-    }
-
-}
